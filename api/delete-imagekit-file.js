93d15e002e4b5cbe72b39f2dbc4b415d99e1d0f7
vardhamanAssociates <associatesvardhaman13@gmail.com>
2025-07-25 09:57:14 +0530

    Update delete-imagekit-file.js
    added cors functionality

--- api/delete-imagekit-file.js
+++ api/delete-imagekit-file.js
@@ -1,4 +1,10 @@
 export default async function handler(req, res) {
+// Enable CORS
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "DELETE, OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  
+// Handle preflight request
   if (req.method !== "DELETE") {
     return res.status(405).json({ error: "Method not allowed" });
   }
