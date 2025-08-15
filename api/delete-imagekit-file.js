f9534476f48f5084fff64adb218d21c1411dc81c
vardhamanAssociates <associatesvardhaman13@gmail.com>
2025-08-15 15:55:03 +0530

    Update delete-imagekit-file.js

--- api/delete-imagekit-file.js
+++ api/delete-imagekit-file.js
@@ -4,6 +4,10 @@ export default async function handler(req, res) {
   res.setHeader("Access-Control-Allow-Methods", "DELETE, OPTIONS");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
 
+   res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  
   // Handle preflight request
   if (req.method === "OPTIONS") {
     return res.status(200).end();
